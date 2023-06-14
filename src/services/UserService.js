629ed1b6efb850fbc12a087a25f12aa69e5fa1d6
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2023-06-14 11:43:48 -0400

    fixed proxy

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,37 +1,41 @@
 import axios from 'axios'
 
-const USERS_REST_API_URL_GET = '/get';
-const USERS_REST_API_URL_POST = '/post';
+// "proxy": "https://springboot-backend.herokuapp.com",
+
+const base = 'https://springboot-backend.herokuapp.com'
+
+const GET = '/get';
+const POST = '/post';
 
 class UserService {
 
     addUser(user) {
       try {
-        axios.post(USERS_REST_API_URL_POST, user)
+        axios.post(base + POST, user)
       } catch (error) {
         console.error(error.response.data); 
       }
     }
 
     getUsers(){
-        return axios.get(USERS_REST_API_URL_GET);
+        return axios.get(base + GET);
     }
 
     getUserInfo(username){
       let url = "/get/" + username;
-      return axios.get(url);
+      return axios.get(base + url);
     }
 
     deleteUserInfo(username){
       let url = "/delete/" + username;
-      return axios.delete(url);
+      return axios.delete(base + url);
     }
     
     async addTicker(ticker, userId) {
       console.log(ticker)
         let url = "/" + userId + "/insert?ticker=" + ticker;
         try {
-            await axios.post(url, null)
+            await axios.post(base + url, null)
           } catch (error) {
             console.error(error.response.data); 
           }
@@ -40,7 +44,7 @@ class UserService {
     async deleteTicker(ticker, userId) {
         let url = "/" + userId + "/delete?ticker=" + ticker;
         try {
-            await axios.delete(url, null)
+            await axios.delete(base + url, null)
           } catch (error) {
             console.error(error.response.data); 
           }
